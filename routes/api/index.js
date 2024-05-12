d0f2fb8a950fcf1042b9b23800bdca5774e44d4c
PhyllisAnnsComputer <plataille6121@gmail.com>
2024-05-12 16:45:42 -0600

    Fixed bugs in categoryRoutes, Product.js, server.js, index.js and deleted and reinstalled package-lock.json and node modules.

--- routes/api/index.js
+++ routes/api/index.js
@@ -7,4 +7,8 @@ router.use("/categories", categoryRoutes);
 router.use("/products", productRoutes);
 router.use("/tags", tagRoutes);
 
+router.use((req, res) => {
+  res.send("<h1>Wrong Route!</h1>");
+});
+
 module.exports = router;
